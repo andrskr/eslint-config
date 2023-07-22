383d278586d89a20d79e32eda814d1854678aecf
Andrii Skrebets <andriiskrebets@gmail.com>
2023-07-22 23:11:47 +0300

    feat: remove project config from ts parser options

--- index.js
+++ index.js
@@ -162,8 +162,6 @@ module.exports = {
       parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
-        project: ['./tsconfig.json', './tsconfig.node.json'],
-        tsconfigRootDir: __dirname,
       },
       plugins: ['@typescript-eslint'],
       extends: [
